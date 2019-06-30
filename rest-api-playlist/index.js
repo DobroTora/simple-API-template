81dc3393722e8b907b41f26ae384aa8b7e69c3bf
Debra Toranska <48626342+WhereNatureEnds@users.noreply.github.com>
2019-07-01 00:12:47 +0200

    handling requests

--- rest-api-playlist/index.js
+++ rest-api-playlist/index.js
@@ -5,8 +5,17 @@ const app = express();
 
 //listening to get request/callback responce to this request
 
-app.get('/', function () {
+// app.get('/', function () {
+//     console.log('GET request');
+// })
+
+//listening to get request/callback responce to this request
+//res, req parameters are ending the request
+//method route callback function
+
+app.get('/api', function (req, res) {
     console.log('GET request');
+    res.send({ name: 'debra' });
 })
 
 //listen for requests
