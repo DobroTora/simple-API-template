8b4856f005e7f3e1c7154c6be918c8fd6faedceb
Debra Toranska <48626342+WhereNatureEnds@users.noreply.github.com>
2019-07-01 00:07:33 +0200

    and GET request

--- rest-api-playlist/index.js
+++ rest-api-playlist/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 //set up express app
 const app = express();
 
+//listening to get request/callback responce to this request
+
+app.get('/', function () {
+    console.log('GET request');
+})
+
 //listen for requests
 app.listen(process.env.port || 4000, function () {
     console.log('now listening for requests');
